983f75640003a248aa6a7fceb4f37f06050d6b8e
Mitchell Stowman <mitchellstowman@gmail.com>
2023-03-21 13:03:09 -0400

    Supply Panel - Created supply panel component with corresponding type and style files - Restructured supplies data into an array for dynamic rendering and the ability to add more resources down the road - Having an issue rendering the icon for each resource

--- backend/supplies.ts
+++ backend/supplies.ts
@@ -1,13 +1,36 @@
-export const supplies = {
-    electricity: 100,   // mWh
-    food:100,           // mK
-    oxygen:100,         // cubic feet
-    water:100,          // liters
-}
+import electricIcon from '../moonager/src/assets/electricity.png';
+import foodIcon from '../moonager/src/assets/food.png';
+import oxygenIcon from '../moonager/src/assets/oxygen.png';
+import waterIcon from '../moonager/src/assets/water.png';
 
-export type supplies = {
-    electricity: number,   // mWh
-    food:number,           // mK
-    oxygen:number,         // cubic feet
-    water:number,          // liters
-};
+export const suppliesArray = [
+    {
+        id: 1,
+        name: 'Electricity',
+        value: 100,
+        units: 'mWh',
+        icon: '',
+    },
+    {
+        id: 2,
+        name: 'Food',
+        value: 100,
+        units: 'Mk',
+
+    },
+    {
+        id: 3,
+        name: 'Oxygen',
+        value: 100,
+        units: 'ft3',
+
+    },
+    {
+        id: 3,
+        name: 'Water',
+        value: 100,
+        units: 'L',
+
+    },
+]
+
---
+++ moonager/src/routes/dashboard/Supplies/supplies.comp.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import '../Supplies/supplies.styles.scss';
+import { suppliesArray } from '../../../../../backend/supplies';
+import { suppliesTypes } from '../Supplies/supplies.types';
+
+export const Supplies = ( props: suppliesTypes ) => {
+    const data = suppliesArray;
+
+    return(
+        <div className='supply-panel'>
+
+            {data.map(item => {
+                return(
+                    
+                <div key={item.id} className='supply-container'>
+                    <img src={item.icon}></img>
+
+                    <div className='supply-values'>
+                        <h3 className='supply-name'>{item.name}</h3>
+                        <h4 className='supply-value'>{item.value} {item.units}</h4>
+                    </div>
+                </div>
+
+                )
+            })}
+
+        </div>
+    )
+}
+
+            
