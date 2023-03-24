04a315c8df8eb9429e9932714a72003ca087e19e
Mitchell Stowman <mitchellstowman@gmail.com>
2023-03-24 08:33:19 -0400

    Cleaned up some things.  Noticed we had components added twice in a few spots.

--- backend/supplies.ts
+++ backend/supplies.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-import electricIcon from '../moonager/src/assets/electricity.png';
+import electricIcon from '../moonager/src/assets/electricy.png';
 import foodIcon from '../moonager/src/assets/food.png';
 import oxygenIcon from '../moonager/src/assets/oxygen.png';
 import waterIcon from '../moonager/src/assets/water.png';
