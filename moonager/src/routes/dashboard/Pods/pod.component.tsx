3987a16292feccc7e91ac5d108c47de261f0e702
Rachel <racheljhumes@gmail.com>
2023-03-23 23:10:48 -0600

    Adding more data to the pod tiles.

--- moonager/src/routes/dashboard/Pods/pod.component.tsx
+++ moonager/src/routes/dashboard/Pods/pod.component.tsx
@@ -2,11 +2,20 @@ import { PodType } from "./pods.types";
 
 export const Pod = ({ pod }: PodType) => {
   const { name, population } = pod;
+  const { electricity, water, oxygen, food } = pod.supplies;
   return (
     <>
       <div className="pod-placeholder">
         <p>{name}</p>
         <p>Population: {population}</p>
+        <ul className="pod-grid-supplies">
+          {" "}
+          Supplies:
+          <li className="pod-grid-supply">{electricity}</li>
+          <li className="pod-grid-supply">{water}</li>
+          <li className="pod-grid-supply">{oxygen}</li>
+          <li className="pod-grid-supply">{food}</li>
+        </ul>
       </div>
       ;
     </>
--- moonager/src/routes/dashboard/Pods/pods.comp.tsx
+++ moonager/src/routes/dashboard/Pods/pods.comp.tsx
@@ -1,6 +1,7 @@
 import { PodListTypes } from "./pods.types";
 import { Pod } from "./pod.component";
 
+
 export const Pods = ({ pods }: PodListTypes) => {
   const allPods = pods.map((pod) => <Pod key={pod.id} pod={pod} />);
   return <div className="tile-wrapper">{allPods}</div>;
